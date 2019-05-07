83193bb9f833150eef6442de5e93fde1716aedfa
sunyang401769@126.com <superdog333>
2019-05-07 11:22:57 +0800

    在线表单设计器

---
+++ squab-web/src/main/java/cn/luminous/squab/controller/form/FormController.java
@@ -0,0 +1,74 @@
+package cn.luminous.squab.controller.form;
+
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.luminous.squab.entity.http.R;
+import cn.luminous.squab.entity.http.Rq;
+import cn.luminous.squab.form.entity.DynamicForm;
+import cn.luminous.squab.form.service.DynamicFormService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/form")
+@Slf4j
+public class FormController {
+
+    @Autowired
+    private DynamicFormService dynamicFormService;
+
+    @RequestMapping("/edit")
+    public String toFormEdit(@RequestParam("formId") Long formId, Model model) {
+        try {
+            DynamicForm dynamicForm = dynamicFormService.queryById(formId);
+            model.addAttribute("id",dynamicForm.getId());
+            model.addAttribute("formName",dynamicForm.getFormName());
+            model.addAttribute("formCode",dynamicForm.getFormCode());
+            model.addAttribute("html",dynamicForm.getFormHtml());
+        }catch (Exception e) {
+            // TODO return 404
+            log.error("【跳转表单编辑器失败】" + e);
+        }
+        return "form/form_edit2";
+    }
+
+    @PostMapping("/saveOrUpdateForm")
+    @ResponseBody
+    public String saveOrUpdateForm(@RequestBody Rq rq) {
+        try {
+            log.debug("【动态表单保存】入参: " + rq.toString());
+            Map<String,Object> data = (Map<String,Object>) rq.getData();
+            String idStr = (String)data.get("id");
+            if (BeanUtil.isEmpty(idStr) || idStr.length()<=0) { // 新增
+                DynamicForm dynamicForm = new DynamicForm();
+                dynamicForm.setFormName((String) data.get("formName"));
+                dynamicForm.setFormName((String) data.get("formCode"));
+                dynamicForm.setFormHtml((String) data.get("template"));
+                dynamicFormService.add(dynamicForm);
+            }else { // 更新
+                DynamicForm dynamicForm = dynamicFormService.queryById(Long.valueOf(idStr));
+                dynamicForm.setFormName((String) data.get("formName"));
+                dynamicForm.setFormName((String) data.get("formCode"));
+                dynamicForm.setFormHtml((String) data.get("template"));
+                dynamicFormService.updateByIdSelective(dynamicForm);
+            }
+        }catch (Exception e) {
+            log.error("【动态表单保存失败】入参: " + rq.toString(), e);
+            return R.nok(e.getMessage());
+        }
+        return R.ok();
+    }
+
+
+
+
+
+
+
+
+}
