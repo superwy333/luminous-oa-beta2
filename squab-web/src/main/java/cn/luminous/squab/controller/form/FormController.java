ec788f035b3ded9aac821cc7110428eabac6c176
superwy333 <Superdog333>
2019-05-09 23:00:58 +0800

    表单

--- squab-web/src/main/java/cn/luminous/squab/controller/form/FormController.java
+++ squab-web/src/main/java/cn/luminous/squab/controller/form/FormController.java
@@ -11,8 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.*;
+import java.util.Date;
 import java.util.Map;
 
 @Controller
@@ -83,6 +87,49 @@ public class FormController {
         model.addAttribute("name","哈哈老王");
 
         return "form/form_preview";
+    }
+
+
+
+
+    /**
+     * 通过流的方式上传文件
+     * 测试文件上传功能
+     * @RequestParam("file") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
+     */
+    @RequestMapping("/fileUpload")
+    @ResponseBody
+    @CrossOrigin
+    public String  fileUpload(@RequestParam("file") MultipartFile file) throws IOException {
+
+
+        //用来检测程序运行时间
+        long  startTime=System.currentTimeMillis();
+        System.out.println("fileName："+file.getOriginalFilename());
+
+        try {
+            //获取输出流
+            OutputStream os=new FileOutputStream("D:/"+new Date().getTime()+file.getOriginalFilename());
+            //获取输入流 CommonsMultipartFile 中可以直接得到文件的流
+            InputStream is=file.getInputStream();
+            int temp;
+            //一个一个字节的读取并写入
+            while((temp=is.read())!=(-1))
+            {
+                os.write(temp);
+            }
+            os.flush();
+            os.close();
+            is.close();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return R.nok();
+        }
+        long  endTime=System.currentTimeMillis();
+        System.out.println("方法一的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        return R.ok();
     }
 
 
