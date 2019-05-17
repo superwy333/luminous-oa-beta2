4b8a07990310c98a20c112d9a0a8c68d41a13d90
sunyang401769@126.com <superdog333>
2019-05-17 10:57:13 +0800

    表单设计器

--- squab-web/src/main/java/cn/luminous/squab/controller/form/model/ueform/field/ListCtrl.java
+++ squab-web/src/main/java/cn/luminous/squab/controller/form/model/ueform/field/ListCtrl.java
@@ -115,66 +115,58 @@ public class ListCtrl extends FormField {
         String name = lc.getName();
 
         String temp_html = type != 1 ? "" : "<script> " +
-                " function tbAddRow(dname) " +
-                "  { " +
-                " var sTbid = dname+'_table';" +
-                " var size = parseInt($('#' + dname + '_length').val());  " +
-                " var fields = $('#' + dname + '_field').val();  " +
-                " var fs =  fields.split('`'); " +
-                "  var table = '<tr>' ; " +
-                " for(var i = 0 ; i < fs.length ; i++) {" +
-                "	 if(fs[i] && fs[i] != '') {" +
-                "		 table += '<td><input class=\"input-medium\" type=\"text\" name=' + dname + '[' + (size+1) + '][' + fs[i] + ']  value=\"\"> </td>';" +
-                " }" +
-                "}  " +
-                "  		 table += '<td><a href=\"javascript:void(0);\" onclick=\"fnDeleteRow(this)\" class=\"delrow show\">删除</a></td>';" +
-                "   table += '</tr>';" +
-                "  $('#' + dname + '_length').val(size+1); " +
-                " $('#'+sTbid).append(table); " +
-                " }" +
-                // 删除tr
-                " function fnDeleteRow(obj)" +
-                " {" +
-                "   var sTbid = '" + name + "_table';" +
-                "   var oTable = document.getElementById(sTbid);" +
-                "   while(obj.tagName !='TR')" +
-                "  {" +
-                "       obj = obj.parentNode;" +
-                "  }" +
-                "  oTable.deleteRow(obj.rowIndex);" +
-                "  var size = $('#" + name + "_length').val(); " +
-                "  $('#" + name + "_length').val(size - 1);  " +
-                " }";
-        temp_html += "//统计\n" +
-                "                            function sum_total(dname, e) {\n" +
-                "                                var tsum = 0;\n" +
+                "function tbAddRow(dname) {\n" +
+                "        var sTbid = dname + \"_table\";\n" +
+                "        var trNum = $(\"#\" + sTbid + \" tbody\").find(\"tr\").length;\n" +
                 "\n" +
-                "                                var tableId = dname + \"_table\";\n" +
-                "                                var inputs = $(\"#\" + tableId).find(\"input\");\n" +
-                "                                //console.log(inputs);\n" +
-                "                                $.each(inputs, function (index, value) {\n" +
-                "                                    var name = $(value).attr(\"name\");\n" +
-                "                                    //alert(name + \">>>>\" + name.indexOf(\"money\"));\n" +
-                "                                    if (name.indexOf(\"money\") != -1) {\n" +
-                "                                        var t = parseFloat($(value).val());\n" +
-                "                                        if (!t) t = 0;\n" +
-                "                                        if (t) tsum += t * 100;\n" +
-                "                                        $(value).val(t);\n" +
-                "                                    }\n" +
-                "                                });\n" +
-                "                                tsum = tsum / 100;\n" +
-                "                                var xsd = tsum.toString().split(\".\");\n" +
-                "                                if (xsd.length == 1) {\n" +
-                "                                    tsum = tsum.toString() + \".00\";\n" +
+                "        $(\"#\" + sTbid + \" .template\")\n" +
+                "        //连同事件一起复制\n" +
+                "            .clone(true)\n" +
+                "            //去除模板标记\n" +
+                "            .removeClass(\"template\").addClass(\"dataRow\" + trNum)\n" +
+                "        //修改内部元素\n" +
+                "            .find(\".delrow\").show().end()\n" +
+                "            .find(\"input\").val(\"\").end()\n" +
+                "            .find(\"textarea\").val(\"\").end()\n" +
+                "        //插入表格\n" +
+                "            .appendTo($(\"#\" + sTbid));\n" +
+                "        // 处理一下name的值\n" +
+                "        var lls = $(\".dataRow\" + trNum).find(\"input\");\n" +
+                "        $.each(lls, function (index, value) {\n" +
+                "            var oldName = $(lls[index]).attr(\"name\");\n" +
+                "            $(lls[index]).attr(\"name\", oldName + \"[\" + trNum + \"]\");\n" +
+                "        })\n" +
+                "    }";
+
+        temp_html += "function sum_total(dname, key) {\n" +
+                "        var tsum = 0;\n" +
                 "\n" +
-                "                                }\n" +
-                "                                if (xsd.length > 1) {\n" +
-                "                                    if (xsd[1].length < 2) {\n" +
-                "                                        tsum = tsum.toString() + \"0\";\n" +
-                "                                    }\n" +
-                "                                }\n" +
-                "                                $('input[name=\"' + dname + '[total]\"]').val(tsum);\n" +
-                "                            }";
+                "        var tableId = dname + \"_table\";\n" +
+                "        var inputs = $(\"#\" + tableId).find(\"input\");\n" +
+                "        $.each(inputs, function (index, value) {\n" +
+                "            var name = $(value).attr(\"name\");\n" +
+                "            //console.log(name + \">>>>\" + name.indexOf(key));\n" +
+                "            if (name != undefined && name.indexOf(key) != -1 && name.indexOf(\"total\") == -1) {\n" +
+                "                var t = parseFloat($(value).val());\n" +
+                "                if (!t) t = 0;\n" +
+                "                if (t) tsum += t * 100;\n" +
+                "                $(value).val(t);\n" +
+                "            }\n" +
+                "        });\n" +
+                "\n" +
+                "        tsum = tsum / 100;\n" +
+                "        var xsd = tsum.toString().split(\".\");\n" +
+                "        if (xsd.length == 1) {\n" +
+                "            tsum = tsum.toString() + \".00\";\n" +
+                "\n" +
+                "        }\n" +
+                "        if (xsd.length > 1) {\n" +
+                "            if (xsd[1].length < 2) {\n" +
+                "                tsum = tsum.toString() + \"0\";\n" +
+                "            }\n" +
+                "        }\n" +
+                "        $('input[name=\"' + dname + \"_\" + key + '[total]\"]').val(tsum);\n" +
+                "    }";
         temp_html += "</script>";
 
 
@@ -245,7 +237,7 @@ public class ListCtrl extends FormField {
                 for (int i = 0; i < fields.length && i < filed_keys.length; i++) {
                     if (filed_keys[i] != null && filed_keys[i].length() > 0) {
                         if ("1".equals(orgsums[i])) { // 这列需要合计
-                            table += "<td><input class=\"input-medium\" type=\"text\" name=\"" + name + "[0][" + filed_keys[i] + "]\" value=\"\" onblur=\"aaa()\"> </td>";
+                            table += "<td><input class=\"input-medium\" type=\"text\" name=\"" + name + "[0][" + filed_keys[i] + "]\" value=\"\" onblur=\"sum_total('" + name + "','" +filed_keys[i] + "')\"> </td>";
                         }else {
                             table += "<td><input class=\"input-medium\" type=\"text\" name=\"" + name + "[0][" + filed_keys[i] + "]\" value=\"\"> </td>";
                         }
@@ -274,7 +266,7 @@ public class ListCtrl extends FormField {
         for (int i = 0; i < orgsums.length; i++) {
             if ("1".equals(orgsums[i])) {
                 Integer ii = Integer.valueOf(i) + 1;
-                table += "<td>合计：<input class=\"input-small\" name=\"" + name + "_" + ii + "[total]\" onblur=\"sum_total('data_5[4][]')\" readonly type=\"text\" value=\"\"></td>";
+                table += "<td>合计：<input class=\"input-small\" name=\"" + name + "_" + filed_keys[i] + "[total]\" onblur=\"sum_total('data_5[4][]')\" readonly type=\"text\" value=\"\"></td>";
             } else {
                 table += "<td></td>";
             }
