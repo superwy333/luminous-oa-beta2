6bd4fea6a61358438f1bd9ed2a9be826fe1fc885
sunyang401769@126.com <superdog333>
2019-05-31 08:29:14 +0800

    增加去掉url中jsession的拦截器

---
+++ squab-web/src/main/java/cn/luminous/squab/filter/Disablejsession.java
@@ -0,0 +1,72 @@
+package cn.luminous.squab.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Component
+@WebFilter(urlPatterns = "/**", filterName = "disablejsession")
+public class Disablejsession implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        // System.out.println("3.过滤器MyDisableUrlSessionFilter");
+        // skip non-http requests
+        if (!(request instanceof HttpServletRequest)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // clear session if session id in URL
+        if (httpRequest.isRequestedSessionIdFromURL()) {
+            HttpSession session = httpRequest.getSession();
+            if (session != null)
+                session.invalidate();
+        }
+
+        // wrap response to remove URL encoding
+        HttpServletResponseWrapper wrappedResponse = new HttpServletResponseWrapper(httpResponse) {
+
+            @Override
+            public String encodeRedirectUrl(String url) {
+                return url;
+            }
+
+            public String encodeRedirectURL(String url) {
+                return url;
+            }
+
+            public String encodeUrl(String url) {
+                return url;
+            }
+
+            public String encodeURL(String url) {
+                return url;
+            }
+        };
+
+        // process next request in chain
+        chain.doFilter(request, wrappedResponse);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
